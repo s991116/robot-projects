e680bbcd572ecf3d7a75d42203fd46f346cabefd
philip.juhl@gmail.com <philip.juhl@gmail.com>
2015-01-17 15:51:17 +0000

    Tilføjet ArduinoController bibliotek

---
+++ Raspberry/Robocup/ArduinoController/src/ComMessage.cpp
@@ -0,0 +1,9 @@
+#include "ComMessage.h"
+
+void ComMessage::GenerateMessage(char command, short data, char* message)
+{
+     message[0] = command;
+     message[1] = (data>>8) & 0x00FF;
+     message[2] = data & 0x00FF;
+}
+
