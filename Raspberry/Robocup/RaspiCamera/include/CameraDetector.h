24d88d4a8728574a79932fcd56a2bae0ca6f2837
philip.juhl@gmail.com <philip.juhl@gmail.com>
2015-01-27 19:25:33 +0000

    include

--- Raspberry/Robocup/RaspiCamera/include/CameraDetector.h
+++ Raspberry/Robocup/RaspiCamera/include/CameraDetector.h
@@ -5,7 +5,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include "RaspiCamCV.h"
+#include <RaspiCamCV.h>
 #include <string.h>
 
 class CameraDetector {
