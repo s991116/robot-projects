cfe33ebb956272b58cab5c447ba18ad5282ebc99
Philip Juhl <philip.juhl@gmail.com>
2015-05-05 22:04:38 +0000

    PID Controller working for segway, missing finetunning

--- Arduino/projects/SegwayController/SegwayController/MotorController/MotorController.ino
+++ Arduino/projects/SegwayController/SegwayController/MotorController/MotorController.ino
@@ -15,19 +15,21 @@
 #define ENCODER_A_INTERRUPT_CHANGE_PIN (2) // Encoder-0 is on DIGITAL PIN 2
 #define ENCODER_B_INTERRUPT_CHANGE_PIN (3) // Encoder-1 is on DIGITAL PIN 3
 
-#define ENCODER_A_DIRECTION_PIN (4) // Encoder-0 Direction is on DIGITAL PIN 4
-#define ENCODER_B_DIRECTION_PIN (5) // Encoder-1 Direction is on DIGITAL PIN 5
+#define MOTOR_B_DIRECTION_PIN (4) //MotorShield Motor B
+#define MOTOR_B_PWM_PIN (5) //MotorShield Motor B
+#define MOTOR_A_PWM_PIN (6) //MotorShield Motor A
+#define MOTOR_A_DIRECTION_PIN (7) //MotorShield Motor A
 
-#define MOTOR_A_DIRECTION_PIN (4) //MotorShield Motor B
-#define MOTOR_A_PWM_PIN (5) //MotorShield Motor B
-#define MOTOR_B_DIRECTION_PIN (7) //MotorShield Motor A
-#define MOTOR_B_PWM_PIN (6) //MotorShield Motor A
+#define ENCODER_A_DIRECTION_PIN (8) // Encoder-0 Direction is on DIGITAL PIN 4
+#define ENCODER_B_DIRECTION_PIN (9) // Encoder-1 Direction is on DIGITAL PIN 5
 
 #define MOTOR_FORWARD (0)
 #define MOTOR_REVERSE (1)
 
-volatile int EncoderATickSpeed = 0;
-volatile int EncoderBTickSpeed = 0;
+#define PID_DIVISION_FACTOR (10000.0)
+
+volatile short EncoderATickSpeed = 0;
+volatile short EncoderBTickSpeed = 0;
 int LastEncoderATickSpeed = 0;
 int LastEncoderBTickSpeed = 0;
 
@@ -39,19 +41,19 @@ int MotorBTargetTickSpeed = 0; //Target ticks pr sample time
 int MotorASpeed = 0; //PWM value in range [-255 ; +255]
 int MotorBSpeed = 0; //PWM value in range [-255 ; +255]
 
-float MotorA_Kp = 26;
-float MotorA_Ki = 16;
+float MotorA_Kp = 0.9;
+float MotorA_Ki = 0;
 float MotorA_Kd = 0;
 
-float MotorB_Kp = 25;
-float MotorB_Ki = 15;
+float MotorB_Kp = 0.9;
+float MotorB_Ki = 0;
 float MotorB_Kd = 0;
 
-PID motorAPID(&LastEncoderATickSpeed, &MotorASpeed, &MotorATargetTickSpeed, MotorA_Kp, MotorA_Ki, MotorA_Kd, DIRECT);
+PID motorAPID(&LastEncoderATickSpeed, &MotorASpeed, &MotorATargetTickSpeed, MotorA_Kp, MotorA_Ki, MotorA_Kd, REVERSE);
 PID motorBPID(&LastEncoderBTickSpeed, &MotorBSpeed, &MotorBTargetTickSpeed, MotorB_Kp, MotorB_Ki, MotorB_Kd, REVERSE);
 
-unsigned long CalculationTime = 0;
-unsigned long SampleTime = 40;
+short CalculationTime = 0;
+short SampleTime = 40;
 int SampleTimeToSmall = false;
 
 bool MotorsRunning = true;
@@ -65,13 +67,9 @@ void setup()
   //Set Direction for Encoder pins
   pinMode(ENCODER_A_DIRECTION_PIN, INPUT);
   pinMode(ENCODER_B_DIRECTION_PIN, INPUT);
-  digitalWrite(ENCODER_A_DIRECTION_PIN, HIGH); //Enable pullup resistor
-  digitalWrite(ENCODER_B_DIRECTION_PIN, HIGH); //Enable pullup resistor
 
   pinMode(ENCODER_A_INTERRUPT_CHANGE_PIN, INPUT);
-  digitalWrite(ENCODER_A_INTERRUPT_CHANGE_PIN, HIGH); //Enable pullup resistor
   pinMode(ENCODER_B_INTERRUPT_CHANGE_PIN, INPUT);
-  digitalWrite(ENCODER_B_INTERRUPT_CHANGE_PIN, HIGH); //Enable pullup resistor
 
   //Set Direction for Motor pins
   pinMode(MOTOR_A_DIRECTION_PIN, OUTPUT);   
@@ -101,6 +99,8 @@ void loop()
 
   UpdateEncoderCounters();
 
+  AddMeasurement(LastEncoderATickSpeed);
+
   motorAPID.Compute();  
   motorBPID.Compute();
   
@@ -123,7 +123,7 @@ void loop()
 
 void EncoderACount()
 {
-  if(digitalReadFast(ENCODER_A_DIRECTION_PIN))// == digitalReadFast(ENCODER_A_INTERRUPT_CHANGE_PIN))
+  if(digitalReadFast(ENCODER_A_DIRECTION_PIN))
     EncoderATickSpeed--;
   else
     EncoderATickSpeed++;
@@ -131,7 +131,7 @@ void EncoderACount()
 
 void EncoderBCount()
 {
-  if(digitalReadFast(ENCODER_B_DIRECTION_PIN))// == digitalReadFast(ENCODER_B_INTERRUPT_CHANGE_PIN))
+  if(digitalReadFast(ENCODER_B_DIRECTION_PIN))
     EncoderBTickSpeed++;
   else
     EncoderBTickSpeed--;
@@ -142,21 +142,21 @@ void SetMotorSpeed(int DirectionPin, int PWMPin, float Speed)
   //Set PWM-Speed and direction for Motor 
   if(Speed > 0)
   {
-    digitalWrite(DirectionPin, MOTOR_FORWARD);
+    digitalWriteFast(DirectionPin, MOTOR_FORWARD);
     analogWrite(PWMPin, Speed);
   }
   else 
   {
-    digitalWrite(DirectionPin, MOTOR_REVERSE);
+    digitalWriteFast(DirectionPin, MOTOR_REVERSE);
     analogWrite(PWMPin, -Speed);
   }
 }
 
 void StopMotors()
 {
-  digitalWrite(MOTOR_A_DIRECTION_PIN, MOTOR_FORWARD);
+  digitalWriteFast(MOTOR_A_DIRECTION_PIN, MOTOR_FORWARD);
   analogWrite(MOTOR_A_PWM_PIN, 0);
-  digitalWrite(MOTOR_B_DIRECTION_PIN, MOTOR_FORWARD);
+  digitalWriteFast(MOTOR_B_DIRECTION_PIN, MOTOR_FORWARD);
   analogWrite(MOTOR_B_PWM_PIN, 0);
 }
 
@@ -164,9 +164,10 @@ void UpdateEncoderCounters()
 {
   LastEncoderATickSpeed = EncoderATickSpeed;
   EncoderATickSpeed = 0;  
+  EncoderATotalTicks += LastEncoderATickSpeed;
+
   LastEncoderBTickSpeed = EncoderBTickSpeed;
   EncoderBTickSpeed = 0;     
-  EncoderATotalTicks += LastEncoderATickSpeed;
   EncoderBTotalTicks += LastEncoderBTickSpeed;
 }
 
@@ -178,15 +179,47 @@ void HandleSerialCommunication()
   if(Serial.available() >= 3)
   {
     command = Serial.read();
-    data = Serial.read();
-    data = data << 8;      //MSB
-    data |= Serial.read(); //LSB
-    
+    data = Serial.read(); //LSB
+    data |= (Serial.read() << 8); //MSB
+
     short response = HandleCommand(command, data);
     
-    Serial.write((byte)(response >> 8));   //MSB
     Serial.write((byte)(response & 0xFF)); //LSB
+    Serial.write((byte)(response >> 8));   //MSB
   }
+}
+
+short nextIndex = 0;
+#define measureArrayLength (500)
+short measureArray[500];
+short MeasureState = 0; //0 = AddMeasure, 1 = Readmeasure
+
+
+void AddMeasurement(short measure)
+{
+  if(MeasureState == 0)
+  {
+    measureArray[nextIndex] = measure;
+    nextIndex++;
+    if(nextIndex >= measureArrayLength)
+    {
+      nextIndex = 0;
+    }
+  } 
+}
+
+short GetMeasurement()
+{
+  if(MeasureState == 1)
+  {
+    nextIndex++;
+    if(nextIndex >= measureArrayLength)
+    {
+      nextIndex = 0;
+    }
+    return measureArray[nextIndex];
+  }
+  return 0;
 }
 
 short HandleCommand(byte command, short data)
@@ -194,32 +227,32 @@ short HandleCommand(byte command, short data)
   switch(command)
   {
     case 1:
-     MotorA_Kp = data;
+     MotorA_Kp = data / PID_DIVISION_FACTOR;
      UpdateMotorAPID();
      break;
       
     case 2:
-     MotorA_Ki = data;
+     MotorA_Ki = data / PID_DIVISION_FACTOR;
      UpdateMotorAPID();
      break;
 
     case 3:
-     MotorA_Kd = data;
+     MotorA_Kd = data / PID_DIVISION_FACTOR;
      UpdateMotorAPID();
      break;
 
     case 4:
-     MotorB_Kp = data;
+     MotorB_Kp = data / PID_DIVISION_FACTOR;
      UpdateMotorBPID();
      break;
 
     case 5:
-     MotorB_Ki = data;
+     MotorB_Ki = data / PID_DIVISION_FACTOR;
      UpdateMotorBPID();
      break;
 
     case 6:
-     MotorB_Kd = data;
+     MotorB_Kd = data / PID_DIVISION_FACTOR;
      UpdateMotorBPID();
      break;
      
@@ -246,7 +279,17 @@ short HandleCommand(byte command, short data)
     case 13:
       MotorsRunning = data;
       break;
-    
+
+    case 14:
+      return CalculationTime;
+      
+    case 15:
+      MeasureState = data;
+      break;
+
+    case 16:
+      return GetMeasurement();
+
     case 255:
       return data;
       break;
