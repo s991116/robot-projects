a502e3da07b2456b62478d446dcedef378119f71
root <root@raspberrypi2.(none)>
2016-12-03 21:04:22 +0000

    modified segway

--- Raspberry/Python/Segway/terminal/control.py
+++ Raspberry/Python/Segway/terminal/control.py
@@ -13,7 +13,8 @@ class control(object):
             
             pidGyro = self.segway.pidGyro();
             pidSpeed = self.segway.pidSpeed();
-
+            pidMotorA = self.segway.pidMotorA();
+            pidMotorB = self.segway.pidMotorB();
             self.myscreen.clear()
             
             self.myscreen.addstr(1, 1, "   _____                   _                    _ ")
@@ -24,9 +25,11 @@ class control(object):
             self.myscreen.addstr(6, 1, "  \_____|  \___/  |_| |_|  \__| |_|     \___/  |_|")
                                                       
             
-            self.myscreen.addstr(9, 1, " TERMINAL")
-            self.myscreen.addstr(10, 1, " PID-Gyro (" + str(pidGyro[0]) + " , " + str(pidGyro[1]) + " , " + str(pidGyro[2]) + ")")
-            self.myscreen.addstr(11, 1, " PID-Speed (" + str(pidSpeed[0]) + " , " + str(pidSpeed[1]) + " , " + str(pidSpeed[2]) + ")")
+            self.myscreen.addstr(8 , 1, " TERMINAL")
+            self.myscreen.addstr(9 , 1, " PID-Gyro (" + str(pidGyro[0]) + " , " + str(pidGyro[1]) + " , " + str(pidGyro[2]) + ")")
+            self.myscreen.addstr(10, 1, " PID-Speed (" + str(pidSpeed[0]) + " , " + str(pidSpeed[1]) + " , " + str(pidSpeed[2]) + ")")
+            self.myscreen.addstr(11, 1, " PID-Motor A (" + str(pidMotorA[0]) + " , " + str(pidMotorA[1]) + " , " + str(pidMotorA[2]) + ")")
+            self.myscreen.addstr(12, 1, " PID-Motor B (" + str(pidMotorB[0]) + " , " + str(pidMotorB[1]) + " , " + str(pidMotorB[2]) + ")")
             
             if(segwayEnableState == 0):
                 self.myscreen.addstr(13, 1, " 1. Enable")
@@ -35,11 +38,13 @@ class control(object):
 
             self.myscreen.addstr(14, 1, " 2. Set PID Gyro")
             self.myscreen.addstr(15, 1, " 3. Set PID Speed")
-            self.myscreen.addstr(16, 1, " R. Wii Remote")
+            self.myscreen.addstr(16, 1, " 4. Set PID Motor A")
+            self.myscreen.addstr(17, 1, " 5. Set PID Motor B")
+            self.myscreen.addstr(18, 1, " R. Wii Remote")
                 
-            self.myscreen.addstr(17, 1, " Esc. Back")
+            self.myscreen.addstr(20, 1, " Esc. Back")
             
-            self.myscreen.addstr(20, 1, " Choice: ")
+            self.myscreen.addstr(22, 1, " Choice: ")
     
             choise = self.myscreen.getch()
             
@@ -53,27 +58,45 @@ class control(object):
                     self.segway.disable()
 
             if(choise == ord('2')):
-                self.myscreen.addstr(22,1, " Gyro P-Corr: ")
-                p = self.myscreen.getstr(22,14, 5)                
-                self.myscreen.addstr(23,1, " Gyro I-Corr: ")
-                i = self.myscreen.getstr(23,14, 5)                
-                self.myscreen.addstr(24,1, " Gyro D-Corr: ")
-                d = self.myscreen.getstr(24,14, 5)                
+                self.myscreen.addstr(25,1, " Gyro P-Corr: ")
+                p = self.myscreen.getstr(25,14, 5)                
+                self.myscreen.addstr(26,1, " Gyro I-Corr: ")
+                i = self.myscreen.getstr(26,14, 5)                
+                self.myscreen.addstr(27,1, " Gyro D-Corr: ")
+                d = self.myscreen.getstr(27,14, 5)                
                 self.segway.SetGyroPid(int(p),int(i),int(d))
                 
             if(choise == ord('3')):
-                self.myscreen.addstr(22,1, " Speed P-Corr: ")
-                p = self.myscreen.getstr(22,15, 5)                
-                self.myscreen.addstr(23,1, " Speed I-Corr: ")
-                i = self.myscreen.getstr(23,15, 5)                
-                self.myscreen.addstr(24,1, " Speed D-Corr: ")
-                d = self.myscreen.getstr(24,15, 5)                
+                self.myscreen.addstr(25,1, " Speed P-Corr: ")
+                p = self.myscreen.getstr(25,15, 5)                
+                self.myscreen.addstr(26,1, " Speed I-Corr: ")
+                i = self.myscreen.getstr(26,15, 5)                
+                self.myscreen.addstr(27,1, " Speed D-Corr: ")
+                d = self.myscreen.getstr(27,15, 5)                
                 self.segway.SetSpeedPid(int(p),int(i),int(d))
+
+            if(choise == ord('4')):
+                self.myscreen.addstr(25,1, " Motor A P-Corr: ")
+                p = self.myscreen.getstr(25,17, 5)                
+                self.myscreen.addstr(26,1, " Motor A I-Corr: ")
+                i = self.myscreen.getstr(26,17, 5)                
+                self.myscreen.addstr(27,1, " Motor A D-Corr: ")
+                d = self.myscreen.getstr(27,17, 5)                
+                self.segway.setMotorAPid(int(p),int(i),int(d))
+
+            if(choise == ord('5')):
+                self.myscreen.addstr(25,1, " Motor B P-Corr: ")
+                p = self.myscreen.getstr(25,17, 5)                
+                self.myscreen.addstr(26,1, " Motor B I-Corr: ")
+                i = self.myscreen.getstr(26,17, 5)                
+                self.myscreen.addstr(27,1, " Motor B D-Corr: ")
+                d = self.myscreen.getstr(27,17, 5)                
+                self.segway.setMotorBPid(int(p),int(i),int(d))
                 
             if(choise == ord('r')):
-                self.myscreen.addstr(22,1, "Press 1 + 2 on Wii to connect")
-                self.myscreen.addstr(23,1, "Press '-' and '+' to disconnect")
-                self.myscreen.addstr(24,1, "")
+                self.myscreen.addstr(25,1, "Press 1 + 2 on Wii to connect")
+                self.myscreen.addstr(26,1, "Press '-' and '+' to disconnect")
+                self.myscreen.addstr(27,1, "")
 
                 self.myscreen.refresh()
                 self.segway.wiiControl()
