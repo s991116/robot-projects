419b111ae666ebefa891e4b983134ce756ebe84f
Philip Juhl <philipjuhl@philips-mbp.home>
2017-12-23 15:22:13 +0100

    Split encoder to one for each wheel

--- Arduino/projects/ArduinoDuoCarController/Controller/Controller.ino
+++ Arduino/projects/ArduinoDuoCarController/Controller/Controller.ino
@@ -1,5 +1,6 @@
 #include <DueMotorEncoder.h>
 #include <DueMotorController.h>
+#include <PID_v1.h>
 
 #define ENCODER_LEFT_1_PIN (50)
 #define ENCODER_LEFT_2_PIN (53)
@@ -14,30 +15,31 @@
 #define MOTOR_2_CONTROL_1_PIN (26)
 #define MOTOR_2_CONTROL_2_PIN (28)
 
-short encoderUpdatePeriod = 10;
+short updatePeriod = 10;
+unsigned long nextUpdateTime;
 
-DueMotorEncoder dueMotorEncoder(ENCODER_LEFT_1_PIN, ENCODER_LEFT_2_PIN, ENCODER_RIGHT_1_PIN, ENCODER_RIGHT_2_PIN, encoderUpdatePeriod);
-DueMotorController dueMotor1Controller(MOTOR_1_SPEED_PIN, MOTOR_1_CONTROL_1_PIN, MOTOR_1_CONTROL_2_PIN, true);
-DueMotorController dueMotor2Controller(MOTOR_2_SPEED_PIN, MOTOR_2_CONTROL_1_PIN, MOTOR_2_CONTROL_2_PIN, false);
+int motor1PIDSetpoint, motor1PIDInput, motor1PIDOutput;
+PID motor1PID(&motor1PIDInput, &motor1PIDOutput, &motor1PIDSetpoint, 0.0, 0.0, 0.0, REVERSE);
+
+DueMotorEncoder dueMotor1Encoder(ENCODER_LEFT_1_PIN, ENCODER_LEFT_2_PIN);
+
+DueMotorController dueMotor1Controller(MOTOR_1_SPEED_PIN, MOTOR_1_CONTROL_1_PIN, MOTOR_1_CONTROL_2_PIN, false);
+DueMotorController dueMotor2Controller(MOTOR_2_SPEED_PIN, MOTOR_2_CONTROL_1_PIN, MOTOR_2_CONTROL_2_PIN, true);
 
 void setup() {
   Serial.begin(9600);
+  motor1PID.SetOutputLimits(-255.0, 255.0);
+
+  motor1PIDSetpoint = 5;
 }
 
 void loop() {
-//  dueMotorEncoder.Update();
-//  Serial.println(dueMotorEncoder.GetSpeedLeft());
-  
-  dueMotor1Controller.SetSpeed(255);
-  dueMotor2Controller.SetSpeed(255);
-  delay(500);
-  dueMotor1Controller.SetSpeed(0);
-  dueMotor2Controller.SetSpeed(0);
-  delay(200);
-  dueMotor1Controller.SetSpeed(-255);
-  dueMotor2Controller.SetSpeed(-255);
-  delay(500);
-  dueMotor1Controller.SetSpeed(0);
-  dueMotor2Controller.SetSpeed(0);
-  delay(200);
+  if(millis() > nextUpdateTime) {
+    dueMotor1Encoder.Compute();
+    motor1PIDInput = dueMotor1Encoder.GetSpeed();
+    motor1PID.Compute();
+    dueMotor1Controller.SetSpeed(motor1PIDOutput);
+
+    nextUpdateTime += updatePeriod;
+  }
 }
