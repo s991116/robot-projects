0b90af46d4372fd6023baaa328e5ce0e46721337
Philip Juhl <philip.juhl@gmail.com>
2019-09-04 20:37:25 +0200

    socket emits and receive(server) navigation

--- SegwayStepperMotor/Raspberry/NodeJS/webserver.js
+++ SegwayStepperMotor/Raspberry/NodeJS/webserver.js
@@ -3,8 +3,13 @@ var chalk = require('chalk');
 var debug = require('debug')('app');
 var morgan = require('morgan');
 var path = require('path');
+var http = require('http');
+var io = require('socket.io');//(express) //require socket.io module and pass the http object (server)
 
 var app = express();
+var httpApp = http.Server(app);
+var ioApp = io(httpApp);
+
 app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname,'public')));
@@ -19,20 +24,13 @@ app.get('/', function(req,res){
   res.sendFile(path.join(__dirname,'public/index.html'));
 })
 
-app.listen(8080, function() {
-  debug(`listening on port ${chalk.green('3000')}`);
-})
-
-/*
-var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
-
-io.sockets.on('connection', function (socket) {// WebSocket Connection
-  var lightvalue = 0; //static variable for current status
-  socket.on('light', function(data) { //get light switch status from client
-    lightvalue = data;
-    if (lightvalue) {
-      console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
-    }
-  });
+ioApp.sockets.on('connection', function (socket) {// WebSocket Connection
+  console.log('Client connected.');
+  socket.on('navigation', (data) => {
+    console.log(data);
+  })
 });
-*/
+
+var server = httpApp.listen(8080, function() {
+  debug(`listening on port ${chalk.green(server.address().port)}`);
+})
