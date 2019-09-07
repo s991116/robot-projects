a419da7d87debf636dfe34a668dfe3341be1d351
Philip Juhl <philip.juhl@gmail.com>
2019-09-07 20:52:15 +0200

    Navigation from clientside to serverside enabled

--- SegwayStepperMotor/Raspberry/NodeJS/SegwayCommunication/arduinoCommunication.js
+++ SegwayStepperMotor/Raspberry/NodeJS/SegwayCommunication/arduinoCommunication.js
@@ -1,18 +1,21 @@
 function arduino() {
+  var chalk = require('chalk');
+  var debug = require('debug')('app');
+  
   var SerialPort = require('serialport');
   var port = new SerialPort('/dev/serial0', { baudRate: 9600 });
   port.on('open', function() {
-      console.log('Port open');
+      debug(chalk.green('Port open'));
   });
 
   port.on('error', function(err) {
-    console.log('Error: ', err.message);
+    debug(chalk.red('Error: ', err.message));
   });
   
   function sendData(cmd,data) {
     port.write([cmd,data], function(err) {
       if (err) {
-        return console.log('Error on write: ', err.message);
+        return debug(chalk.red('Error: ', err.message));
       }
     });
   }
@@ -37,13 +40,3 @@ function arduino() {
     getData: getData,
   }
 }
-
-//TEST
-// let a = arduino();
-// a.sendData(0x10,123)
-// a.getData(0x20).then( data => {
-//   console.log(data);
-//   a.sendData(0x10,23);
-//   a.getData(0x20).then( data => console.log(data));
-// })
-
