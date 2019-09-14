69a0bdadd018a5a5a41ef4241da3e226a9a9e7c1
Philip Juhl <philip.juhl@gmail.com>
2019-09-14 22:08:48 +0200

    video streaming

---
+++ SegwayStepperMotor/Raspberry/NodeJS/ffmpeg.sh
@@ -0,0 +1,7 @@
+node websocket-relay.js supersecret 8081 8082 &
+ffmpeg \
+	-f v4l2 \
+		-framerate 25 -video_size 640x480 -i /dev/video0 \
+	-f mpegts \
+		-codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 \
+	http://localhost:8081/supersecret
